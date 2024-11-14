db35b36decc0b52a60ac8503a11aece471301bbf
SamsShow <sakshamtyagi2008@gmail.com>
2024-11-14 13:02:51 +0530

    ✅ Initialize React project with Vite setup and basic components

---
+++ react-test/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import reactLogo from '../assets/react.svg';
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div>
+            <h1>Saksham Tyagi</h1>
+            <h2>Roll No: 2200320230041</h2>
+            <img src={reactLogo} alt="logo" />
+        </div>
+    );
+};
+
+export default Card;
